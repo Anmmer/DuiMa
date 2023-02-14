b126474e0a9fb8855acbc7c1693885e3d336ae84
changan <2868310944@qq.com>
2023-02-14 19:04:26 +0800

    commit

--- src/main/java/GetPieData.java
+++ src/main/java/GetPieData.java
@@ -36,22 +36,31 @@ public class GetPieData extends HttpServlet {
             int i = 1;
             int j = 0;
             con = DbUtil.getCon();
-            String sql = "select build_type name,sum(fangliang) value from preproduct a left join plan b on a.plannumber = b.plannumber  where a.pourmade = 1 and  b.planname = ?";
-            String sql2 = "select build_type name,sum(fangliang) value from preproduct a left join plan b on a.plannumber = b.plannumber where a.inspect = 1 and  b.planname = ?";
+            String sql = "select build_type name,sum(fangliang) value from preproduct a  where a.pourmade = 1 and isdelete = 0 and  a.planname = ?";
+            String sql2 = "select build_type name,sum(fangliang) value from preproduct a  where a.inspect = 1 and isdelete = 0 and  a.planname = ?";
+            String sql3 = "select build_type name,count(*) value from preproduct a  where   isdelete = 0 and  a.planname = ?";
+            String sql4 = "select build_type name,sum(fangliang) value from preproduct a  where  isdelete = 0 and  a.planname = ?";
             if (building_no != null && !"".equals(building_no)) {
                 sql += " and building_no = ?";
                 sql2 += " and building_no = ?";
+                sql3 += " and building_no = ?";
+                sql4 += " and building_no = ?";
                 i++;
             }
 
             if (floor_no != null && !"".equals(floor_no)) {
                 sql += " and floor_no = ?";
                 sql2 += " and floor_no = ?";
+                sql3 += " and floor_no = ?";
+                sql4 += " and floor_no = ?";
                 i++;
             }
             j = i;
             sql += " group by build_type";
             sql2 += " group by build_type";
+            sql3 += " group by build_type";
+            sql4 += " group by build_type";
+
             ps = con.prepareStatement(sql);
             if (floor_no != null && !"".equals(floor_no)) {
                 ps.setString(i--, floor_no);
@@ -68,14 +77,16 @@ public class GetPieData extends HttpServlet {
                 map.put("value", rs.getString("value"));
                 list.add(map);
             }
+
+            i = j;
             ps = con.prepareStatement(sql2);
             if (floor_no != null && !"".equals(floor_no)) {
-                ps.setString(j--, floor_no);
+                ps.setString(i--, floor_no);
             }
             if (building_no != null && !"".equals(building_no)) {
-                ps.setString(j--, building_no);
+                ps.setString(i--, building_no);
             }
-            ps.setString(j, planname);
+            ps.setString(i, planname);
             rs = ps.executeQuery();
             List<Map<String, String>> list2 = new ArrayList<>();
             while (rs.next()) {
@@ -84,8 +95,46 @@ public class GetPieData extends HttpServlet {
                 map.put("value", rs.getString("value"));
                 list2.add(map);
             }
+
+            i = j;
+            ps = con.prepareStatement(sql3);
+            if (floor_no != null && !"".equals(floor_no)) {
+                ps.setString(i--, floor_no);
+            }
+            if (building_no != null && !"".equals(building_no)) {
+                ps.setString(i--, building_no);
+            }
+            ps.setString(i, planname);
+            rs = ps.executeQuery();
+            List<Map<String, String>> list3 = new ArrayList<>();
+            while (rs.next()) {
+                Map<String, String> map = new HashMap<>();
+                map.put("name", rs.getString("name"));
+                map.put("value", rs.getString("value"));
+                list3.add(map);
+            }
+
+            i = j;
+            ps = con.prepareStatement(sql4);
+            if (floor_no != null && !"".equals(floor_no)) {
+                ps.setString(i--, floor_no);
+            }
+            if (building_no != null && !"".equals(building_no)) {
+                ps.setString(i--, building_no);
+            }
+            ps.setString(i, planname);
+            rs = ps.executeQuery();
+            List<Map<String, String>> list4 = new ArrayList<>();
+            while (rs.next()) {
+                Map<String, String> map = new HashMap<>();
+                map.put("name", rs.getString("name"));
+                map.put("value", rs.getString("value"));
+                list4.add(map);
+            }
             result.put("pie1", list);
             result.put("pie2", list2);
+            result.put("pie3", list3);
+            result.put("pie4", list4);
             out.write(JSON.toJSONString(result));
         } catch (Exception e) {
             e.printStackTrace();
